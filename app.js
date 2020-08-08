ddd6a997ba17f4c0970f213988e6b9f186d448c4
xaviervitor <xavier1vitor@gmail.com>
2020-08-08 01:26:59 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,62 @@
+// Globals
+var cellMatrix = [];
+
+// Element definitions 
+const formConfig = document.getElementById("form-config");
+const inputTextFieldSize = document.getElementById("inputtext-field-size");
+const inputTextBombNumber = document.getElementById("inputtext-bomb-number");
+const divField = document.getElementById("div-field");
+
+// Event binders
+formConfig.addEventListener('submit', function() {
+    event.preventDefault();
+    setPlayField(inputTextFieldSize.value, inputTextBombNumber.value);
+});
+
+// Functions
+function getRandomCell(fieldSize) {
+    return Math.floor(Math.random() * fieldSize);
+}
+
+function setPlayField(fieldSize, bombNumber) {
+    if (!fieldSize)
+        return;
+
+    // Cria campo limpo
+    for (let i = 0 ; i < fieldSize ; i++) {
+        for (let j = 0 ; j < fieldSize ; j++) {
+            cellMatrix[i] = [];
+            cellMatrix[i][j] = 0;
+        }
+    }
+
+    // Gera 10 bombas
+    for (let i = 0 ; i < bombNumber ; i++) {
+        cellMatrix[getRandomCell(fieldSize)][getRandomCell(fieldSize)] = -1;
+    }
+
+    // Renderiza campo
+    divField.innerHTML = '';
+    for (let i = 0 ; i < inputTextFieldSize.value ; i++) {
+        let divRow = document.createElement('div');
+        for (let j = 0 ; j < inputTextFieldSize.value ; j++) {
+            let button = document.createElement('button');
+            button.classList.add('cell');
+            button.addEventListener('click', function(){
+                onCellClick(i, j);
+            });
+            button.disabled = true;
+            if (cellMatrix[i][j] == -1) {
+                button.innerText = 'B';
+            } else {
+                button.innerHTML = '&nbsp;';
+            }
+            divRow.appendChild(button);
+        }
+        divField.appendChild(divRow);
+    }
+}
+
+function onCellClick() {
+
+}
